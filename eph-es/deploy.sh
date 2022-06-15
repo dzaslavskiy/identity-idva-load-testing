0736f361860b4abe5bf485f12e132b489f086b6e
dzaslavskiy <dzaslavskiy@users.noreply.github.com>
2022-06-15 16:14:37 -0400

    update eph-es install script (#34)
    * update eph-es install script
    
    * add target command to ensure deployment target correct env
    
    * fix template json
    
    * change app route

--- eph-es/deploy.sh
+++ eph-es/deploy.sh
@@ -7,6 +7,7 @@ secrets_file="sk-secrets-dev.json"
 echo "press key to continue to next step:"
 read -rp "1. Deploy elasticsearch app"
 
+cf target -s "$env"
 cf push --vars-file ./vars.yaml --var ENVIRONMENT=$env
 
 read -rp "2. Network policies"
@@ -19,7 +20,7 @@ cf add-network-policy sk-analytics eph-es --protocol tcp --port 61443
 read -rp "3. configure secrets"
 
 # settings es configuration in sk secrets to point to eph-es
-jq --arg env "$env" '.es.usePath = false | .es.url = "identity-idva-es-\($env).apps.internal:61443" | .es.accessKeyId = "id" | .es.secretAccessKey = "sec"' "$secrets_file" > sk-secrets-es.json
+jq --arg env "$env" '.es.usePath = false | .es.url = "idva-eph-es-\($env).apps.internal:61443" | .es.accessKeyId = "id" | .es.secretAccessKey = "sec"' "$secrets_file" > sk-secrets-es.json
 
 read -rp "4. configure sk-esconfigs, sk-events, sk-events-read, sk-analytics"
 
