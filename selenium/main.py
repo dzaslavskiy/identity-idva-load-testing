bb81bbd7cf867d40ab47f0422754f8da5ca40d22
dzaslavskiy <dzaslavskiy@easydynamics.com>
2022-07-15 00:34:01 -0400

    test selenium

---
+++ selenium/main.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+options = FirefoxOptions()
+
+driver = webdriver.Firefox(
+    service=FirefoxService(GeckoDriverManager().install()), options=options
+)
+
+driver.get("https://google.com")
+
+title = driver.title
+assert title == "Google"
+
+driver.implicitly_wait(0.5)
+
+search_box = driver.find_element(by=By.NAME, value="q")
+search_button = driver.find_element(by=By.NAME, value="btnK")
+
+search_box.send_keys("Selenium\n")
+search_button.click()
+
+search_box = driver.find_element(by=By.NAME, value="q")
+value = search_box.get_attribute("value")
+assert value == "Selenium"
+
+driver.quit()
